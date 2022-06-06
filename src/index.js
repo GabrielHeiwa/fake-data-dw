43e4fa5b46b82cd8794df51fb4fbea58243a8420
Gabriel Paz Dos Santos <gabriel.paz@beansoftwares.com.br>
2022-06-05 21:32:07 -0300

    FINISH: Versão final do fake data para o novo e último banco de dados para DW

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import faker from "@faker-js/faker";
-import prisma from "@prisma/client";
+import faker from "@faker-js/faker"
+import prisma from "@prisma/client"
 
-const CARGOS = 10;
-const CLIENTES = 100;
-const COLABORADORES = 10;
-const FORNECEDORES = 5;
-const FORNECEDORES_PRODUTOS = 50;
-const PEDIDOS = 90;
-const PEDIDOS_PRODUTOS = 90;
-const PRODUTOS = 50;
-const NOTAS_FISCAIS = 140
+const CARGOS = 7
+const CLIENTES = 100
+const COLABORADORES = 15
+const FORNECEDORES = 15
+const PEDIDOS_CLIENTES = 70
+const PEDIDOS_CLIENTES_PRODUTOS = 70
+const PEDIDOS_FORNECEDORES = 100
+const PEDIDOS_FORNECEDORES_PRODUTOS = 100
+const PRODUTOS = 90
+const NOTAS_FISCAIS = 170
 
 faker.setLocale("pt_BR");
 
@@ -20,48 +21,60 @@ const dados = {
   colaboradores: [],
   fornecedores: [],
   notas_fiscais: [],
-  fornecedores_produtos: [],
-  pedido_produtos: [],
-  pedidos: [],
-  produtos: []
+  produtos: [],
+  pedidos_clientes: [],
+  pedidos_fornecedores: [],
+  pedidos_clientes_produtos: [],
+  pedidos_fornecedores_produtos: [],
 };
 
 async function main() {
-  console.time("start");
+  console.time("start")
 
-  await client.pedido_produtos.deleteMany()
-  await client.pedidos.deleteMany()
-  await client.fornecedores_produtos.deleteMany()
-  await client.notas_fiscais.deleteMany()
-  await client.produtos.deleteMany()
-  await client.fornecedores.deleteMany()
-  await client.clientes.deleteMany()
-  await client.colaboradores.deleteMany()
-  await client.cargos.deleteMany()
+  await delete_all_tables()
+  await generate_fake_data()
+  await populate_all_tables()
 
+  console.timeEnd("start")
 
-  await fake_cargos();
-  await fake_colaboradores();
-  await fake_clientes();
-  await fake_fornecedores();
-  await fake_produtos();
-  await fake_nota_fiscais();
-  await fake_fornecedores_produtos();
-  await fake_pedidos();
-  await fake_pedidos_produtos();
+  async function generate_fake_data() {
+    await fake_cargos()
+    await fake_colaboradores()
+    await fake_clientes()
+    await fake_fornecedores()
+    await fake_nota_fiscais()
+    await fake_produtos()
+    await fake_pedidos_clientes()
+    await fake_pedidos_clientes_produtos()
+    await fake_pedidos_fornecedores()
+    await fake_pedidos_fornecedores_produtos()
+  }
 
+  async function delete_all_tables() {
+    await client.pedidos_clientes_produtos.deleteMany()
+    await client.pedidos_fornecedores_produtos.deleteMany()
+    await client.pedidos_clientes.deleteMany()
+    await client.pedidos_fornecedores.deleteMany()
+    await client.produtos.deleteMany()
+    await client.notas_fiscais.deleteMany()
+    await client.fornecedores.deleteMany()
+    await client.clientes.deleteMany()
+    await client.colaboradores.deleteMany()
+    await client.cargos.deleteMany()
+  }
 
-  await client.notas_fiscais.createMany({ data: dados['notas_fiscais'] })
-  await client.cargos.createMany({ data: dados['cargos'] })
-  await client.colaboradores.createMany({ data: dados['colaboradores'] })
-  await client.clientes.createMany({ data: dados['clientes'] })
-  await client.fornecedores.createMany({ data: dados['fornecedores'] })
-  await client.produtos.createMany({ data: dados['produtos'] })
-  await client.pedidos.createMany({ data: dados['pedidos'] })
-  await client.pedido_produtos.createMany({ data: dados['pedido_produtos'] })
-  await client.fornecedores_produtos.createMany({ data: dados['fornecedores_produtos'] })
-
-  console.timeEnd("start");
+  async function populate_all_tables() {
+    await client.cargos.createMany({ data: dados['cargos'] })
+    await client.colaboradores.createMany({ data: dados['colaboradores'] })
+    await client.clientes.createMany({ data: dados['clientes'] })
+    await client.fornecedores.createMany({ data: dados['fornecedores'] })
+    await client.notas_fiscais.createMany({ data: dados['notas_fiscais'] })
+    await client.produtos.createMany({ data: dados['produtos'] })
+    await client.pedidos_clientes.createMany({ data: dados['pedidos_clientes'] })
+    await client.pedidos_fornecedores.createMany({ data: dados['pedidos_fornecedores'] })
+    await client.pedidos_clientes_produtos.createMany({ data: dados['pedidos_clientes_produtos'] })
+    await client.pedidos_fornecedores_produtos.createMany({ data: dados['pedidos_fornecedores_produtos'] })
+  }
 }
 
 main();
@@ -70,7 +83,6 @@ async function fake_cargos() {
   console.time("Gerando cargos...");
 
   for (let i = 0; i < CARGOS; i++) {
-    // @ts-ignore
     dados["cargos"].push({
       id: faker.datatype.uuid(),
       nome: faker.word.adjective(),
@@ -84,10 +96,8 @@ async function fake_colaboradores() {
   console.time("Gerando colaboradores...");
 
   for (let i = 0; i < COLABORADORES; i++) {
-    // @ts-ignore
     const cargo_id = dados["cargos"][faker.datatype.number({ min: 0, max: CARGOS - 1 })].id;
 
-    // @ts-ignore
     dados["colaboradores"].push({
       id: faker.datatype.uuid(),
       nome: faker.name.findName(),
@@ -110,7 +120,6 @@ async function fake_clientes() {
   console.time("Gerando clientes...");
 
   for (let i = 0; i < CLIENTES; i++) {
-    // @ts-ignore
     dados["clientes"].push({
       id: faker.datatype.uuid(),
       nome: faker.name.findName(),
@@ -129,7 +138,6 @@ async function fake_fornecedores() {
   console.time("Gerando fornecedores...");
 
   for (let i = 0; i < FORNECEDORES; i++) {
-    // @ts-ignore
     dados["fornecedores"].push({
       id: faker.datatype.uuid(),
       nome: faker.company.companyName(),
@@ -143,18 +151,12 @@ async function fake_fornecedores() {
 async function fake_nota_fiscais() {
   console.time("Gerando notas fiscais...")
 
-
   for (let i = 0; i < NOTAS_FISCAIS; i++) {
     let codigo
     do {
-
       codigo = faker.datatype.number({ min: 1, max: 5000 }).toString()
-    } while (
-      // @ts-ignore
-      dados['notas_fiscais'].findIndex(note => note.codigo === codigo) !== -1
-    )
+    } while (dados['notas_fiscais'].findIndex(note => note.codigo === codigo) !== -1)
 
-    // @ts-ignore
     dados['notas_fiscais'].push({
       id: faker.datatype.uuid(),
       codigo,
@@ -169,7 +171,6 @@ async function fake_produtos() {
   console.time("Gerando produtos...");
 
   for (let i = 0; i < PRODUTOS; i++) {
-    // @ts-ignore
     dados["produtos"].push({
       id: faker.datatype.uuid(),
       nome: faker.commerce.product(),
@@ -181,61 +182,29 @@ async function fake_produtos() {
   console.timeEnd("Gerando produtos...");
 }
 
-async function fake_fornecedores_produtos() {
-  console.time("Gerando fornecedores_produtos...")
-
-  for (let i = 0; i < FORNECEDORES_PRODUTOS; i++) {
-
-    let nota_fiscal
-    do {
-      nota_fiscal = dados['notas_fiscais'][faker.datatype.number({ min: 0, max: NOTAS_FISCAIS - 1 })].codigo
-    } while (
-      dados['fornecedores_produtos'].findIndex(pp => pp.nota_fiscal === nota_fiscal) !== -1
-    )
-
-    for (let j = 0; j < 3; j++) {
-      const produto = dados['produtos'][faker.datatype.number({ min: 0, max: PRODUTOS - 1 })].id
-
-      dados['fornecedores_produtos'].push({
-        id: faker.datatype.uuid(),
-        quantidade: faker.datatype.number({ min: 1, max: 1000 }),
-        preco: parseFloat(faker.finance.amount(1, 1000, 2)),
-        produto,
-        nota_fiscal,
-      })
-    }
-  }
-
-  console.timeEnd("Gerando fornecedores_produtos...")
-}
-
-async function fake_pedidos() {
+async function fake_pedidos_clientes() {
   console.time('Gerando os pedidos...')
 
-  const formas_de_pagamento = ['cartão de crédito', 'cartão de débito', 'dinheiro']
-
-  for (let i = 0; i < PEDIDOS; i++) {
-    // @ts-ignore
-    const colaborador = dados['colaboradores'][faker.datatype.number({ min: 0, max: COLABORADORES - 1 })].id
-    // @ts-ignore
-    const cliente = dados['clientes'][faker.datatype.number({ min: 0, max: CLIENTES - 1 })].id
-
-
-    const forma_de_pagamento = formas_de_pagamento[faker.datatype.number({ min: 0, max: 2 })]
+  for (let i = 0; i < PEDIDOS_CLIENTES; i++) {
+    const colaborador_id = dados['colaboradores'][faker.datatype.number({ min: 0, max: COLABORADORES - 1 })].id
+    const cliente_id = dados['clientes'][faker.datatype.number({ min: 0, max: CLIENTES - 1 })].id
 
     let codigo
     do {
       codigo = faker.datatype.number({ min: 0, max: 1000 }).toString()
+    } while (dados['pedidos_clientes'].findIndex(request => request.codigo === codigo) !== -1);
 
-      // @ts-ignore
-    } while (dados['pedidos'].findIndex(request => request.codigo === codigo) !== -1);
+    let notas_fiscais_codigo
+    do {
+      notas_fiscais_codigo = dados['notas_fiscais'][faker.datatype.number({ min: 0, max: NOTAS_FISCAIS - 1 })].codigo
+    } while (dados['pedidos_clientes'].findIndex(notes => notes.notas_fiscais_codigo === notas_fiscais_codigo) !== -1)
 
-    // @ts-ignore
-    dados['pedidos'].push({
+    dados['pedidos_clientes'].push({
       id: faker.datatype.uuid(),
-      colaborador,
-      cliente,
-      forma_de_pagamento,
+      colaborador_id,
+      cliente_id,
+      codigo,
+      notas_fiscais_codigo,
       data: faker.date.recent(faker.datatype.number({ min: 1, max: 90 }))
     })
   }
@@ -243,40 +212,89 @@ async function fake_pedidos() {
   console.timeEnd('Gerando os pedidos...')
 }
 
-async function fake_pedidos_produtos() {
-  console.time('Gerando os produtos dos pedidos...')
+async function fake_pedidos_fornecedores() {
+  console.time("Gerando pedidos_fornecedores...")
 
-  for (let i = 0; i < PEDIDOS_PRODUTOS; i++) {
-    const pedido = dados['pedidos'][faker.datatype.number({ min: 0, max: PEDIDOS - 1 })].id
+  for (let i = 0; i < PEDIDOS_FORNECEDORES; i++) {
 
-    let nota_fiscal
+    const colaborador_id = dados['colaboradores'][faker.datatype.number({ min: 0, max: COLABORADORES - 1 })].id
+    const fornecedor_id = dados['fornecedores'][faker.datatype.number({ min: 0, max: FORNECEDORES - 1 })].id
 
+    let codigo
     do {
-      // @ts-ignore
-      nota_fiscal = dados['notas_fiscais'][faker.datatype.number({ min: 0, max: NOTAS_FISCAIS - 1 })].codigo
+      codigo = faker.datatype.number({ min: 0, max: 1000 }).toString()
+    } while (dados['pedidos_fornecedores'].findIndex(request => request.codigo === codigo) !== -1);
 
-    } while (
-      // @ts-ignore
-      dados['pedido_produtos'].findIndex(pp => pp.nota_fiscal === nota_fiscal) !== -1 &&
+    let notas_fiscais_codigo
+    do {
+      notas_fiscais_codigo = dados['notas_fiscais'][faker.datatype.number({ min: 0, max: NOTAS_FISCAIS - 1 })].codigo
+    } while (dados['pedidos_fornecedores'].findIndex(notes => notes.notas_fiscais_codigo === notas_fiscais_codigo) !== -1)
 
-      // @ts-ignore
-      dados['fornecedores_produtos'].findIndex(fp => fp.nota_fiscal === nota_fiscal) !== -1
-    )
+    dados['pedidos_fornecedores'].push({
+      id: faker.datatype.uuid(),
+      fornecedor_id,
+      colaborador_id,
+      data: faker.date.recent(faker.datatype.number({ min: 1, max: 90 })),
+      notas_fiscais_codigo,
+      codigo
+    })
+  }
 
-    for (let j = 0; j < faker.datatype.number({ min: 1, max: 6 }); j++) {
-      const produto = dados['produtos'][faker.datatype.number({ min: 0, max: PRODUTOS - 1 })].id
+  console.timeEnd("Gerando pedidos_fornecedores...")
+}
 
-      dados['pedido_produtos'].push({
+async function fake_pedidos_clientes_produtos() {
+  console.time('Gerando pedidos_clientes_produtos...')
+
+  for (let i = 0; i < PEDIDOS_CLIENTES_PRODUTOS; i++) {
+    let pedidos_clientes_codigo
+    do {
+      pedidos_clientes_codigo = dados['pedidos_clientes'][faker.datatype.number({ min: 0, max: PEDIDOS_CLIENTES - 1 })].codigo
+    } while (dados['pedidos_clientes_produtos'].findIndex(pcp => pcp.pedidos_clientes_codigo === pedidos_clientes_codigo) !== -1)
+
+    const productQuantity = faker.datatype.number({ min: 1, max: 6 })
+
+    for (let j = 0; j < productQuantity; j++) {
+      const produto = dados['produtos'][faker.datatype.number({ min: 0, max: PRODUTOS - 1 })]
+      const quantidade = faker.datatype.number({ min: 1, max: 3 })
+      dados['pedidos_clientes_produtos'].push({
         id: faker.datatype.uuid(),
-        pedido,
-        produto,
-        quantidade: faker.datatype.number({ min: 1, max: 3 }),
-        nota_fiscal,
+        produto_id: produto.id,
+        quantidade,
+        pedidos_clientes_codigo,
+        total: produto.preco * quantidade
       })
     }
   }
 
-  console.timeEnd('Gerando os produtos dos pedidos...')
+  console.timeEnd('Gerando pedidos_clientes_produtos...')
 }
 
+async function fake_pedidos_fornecedores_produtos() {
+  console.time("Gerando pedidos_fornecedores_produtos...")
 
+  for (let i = 0; i < PEDIDOS_FORNECEDORES_PRODUTOS; i++) {
+    let pedidos_fornecedores_codigo
+    do {
+      pedidos_fornecedores_codigo = dados['pedidos_fornecedores'][faker.datatype.number({ min: 0, max: PEDIDOS_FORNECEDORES - 1 })].codigo
+    } while (dados['pedidos_fornecedores_produtos'].findIndex(pfp => pfp.pedidos_fornecedores_codigo === pedidos_fornecedores_codigo) !== -1)
+
+    const productQuantity = faker.datatype.number({ min: 1, max: 6 })
+
+    for (let j = 0; j < productQuantity; j++) {
+      const produto = dados['produtos'][faker.datatype.number({ min: 0, max: PRODUTOS - 1 })]
+      const quantidade = faker.datatype.number({ min: 1, max: 3 })
+
+      dados['pedidos_fornecedores_produtos'].push({
+        id: faker.datatype.uuid(),
+        produto_id: produto.id,
+        quantidade,
+        total: produto.preco * quantidade,
+        pedidos_fornecedores_codigo,
+      })
+    }
+
+  }
+  console.timeEnd("Gerando pedidos_fornecedores_produtos...")
+}
+// 5aSc6C72
